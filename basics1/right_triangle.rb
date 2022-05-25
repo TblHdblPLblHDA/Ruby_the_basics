a3347316163d56063e04676fe7dd6fa1c96742ff
Red <valer4sivaev234@gmail.com>
2022-05-25 21:39:26 +0300

    basic1

--- basics1/right_triangle.rb
+++ basics1/right_triangle.rb
@@ -13,13 +13,13 @@ else
   hypotenuse = a
 end
 
-if hypotenuse ** 2 = a ** 2 + b ** 2 || hypotenuse ** 2 = b ** 2 + c ** 2 || hypotenuse ** 2 = a ** 2 + c ** 2 
+if hypotenuse**2 = a**2 + b**2 || hypotenuse**2 = b**2 + c**2 || hypotenuse**2 = a**2 + c**2 
   puts "triangle rectangular"
 
 elsif a == b && b == c && a == c
   print "triangle equilateral and isosceles"
 elsif  (a == b || a == c || b == c) && (a != b || b != c || a != c) 
-  puts " triangle isosceles"
+  puts "triangle isosceles"
 else
   print "another isosceles"
 end
--- basics1/the_quadratic_equation.rb
+++ basics1/the_quadratic_equation.rb
@@ -5,19 +5,19 @@ puts "Введите b"
 puts "Введите c"
   c = gets.to_i
 
-d = b ** 2 - 4 * a * c
-puts "d = b ** 2 - 4 * a * c"
+d = b**2 - 4 * a * c
+puts "d = b**2 - 4 * a * c"
 
-if d<0
+if d < 0
   print "Корней нет"
 elsif
-  x1 = (-b + Math.sqrt(d)) / (2*a)
-  x2 = (-b - Math.sqrt(d)) / (2*a)
-  puts "x1 = -b + Math.sqrt(d)) / (2*a)
+  x1 = (-b + Math.sqrt(d)) / (2 * a)
+  x2 = (-b - Math.sqrt(d)) / (2 * a)
+  puts "x1 = -b + Math.sqrt(d)) / (2 * a)
   x2 = -b - Math.sqrt(d)) / (2*a)"
 
-  puts "x1 = -#{b} + Math.sqrt(#{d})) / (2*#{a})
-  x2 = -#{b} - Math.sqrt(#{d})) / (2*#{a})"
+  puts "x1 = (-#{b} + Math.sqrt(#{d})) / (2 * #{a})
+  x2 = (-#{b} - Math.sqrt(#{d})) / (2 * #{a}) "
 
   puts "x1 = #{x1} , x2 = #{x2} "
 else d == 0
