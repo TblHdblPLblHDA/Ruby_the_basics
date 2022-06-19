2efc566abf53e296de76ec4b3b38fd6a0fb15fad
Lyambda <valer4sivaev234@gmail.com>
2022-06-19 13:34:27 +0300

    fix

---
+++ lesson4/main.rb
@@ -0,0 +1,15 @@
+require_relative 'station'
+require_relative 'route'
+require_relative 'trains'
+require_relative 'passenger_train'
+require_relative 'cargo_train'
+require_relative 'railway_wagon'
+require_relative 'railway_wagon_passenger'
+require_relative 'railway_wagon_cargo'
+require_relative 'interface'
+
+interface = Interface.new
+
+loop do
+  interface.start
+end
